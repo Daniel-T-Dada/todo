6f63ff7b8a2dbe77a99e57897b1aaf760d1e90da
Double D <simplytobs@gmail.com>
2024-10-14 00:37:09 +0100

    unmark and delete functionality

--- todo_main/views.py
+++ todo_main/views.py
@@ -5,12 +5,12 @@ from todos.models import Task
 
 def index(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+
+    completed_tasks = Task.objects.filter(is_completed=True)
     context = {
         'tasks': tasks,
+        'completed_tasks': completed_tasks
     }
     return render(request, 'index.html', context)
 
 
-# def index(request):  # +
-#     tasks = Task.objects.filter(is_completed=False)  # +
-#     return render(request, 'index.html', {'tasks': tasks})  # +
